44bd88c31e7fd93a4387b2c7520fe21b2729be53
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-08 18:36:24 +0000

    class to mamange the API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Create a class to manage the API authentication
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Class template for all authentication system to implement
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Placeholder for requiring authentication
+        Args:
+          path (str): The requested path
+          excluded_paths (List[str]): List of excluded paths
+        Returns:
+          - bool: False for now
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Placeholder for getting the authorization header
+        Args:
+          request (Flask request): The flask request object
+        Returns:
+          - str: None for now
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder for getting the current user
+        Args:
+          request (Flask request): The flask request object
+        Returns:
+          TypeVar('User'): None for now
+        """
+        return None
