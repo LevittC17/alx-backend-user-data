5ad7ab849d7cdb6e6c9d062550d787e0bab9a26c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-10 10:54:13 +0000

    Basic Auth - extracting Base64 of Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,4 +13,16 @@ class BasicAuth(Auth):
     Returns:
       - Empty for now
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header
+        for Basic Auth
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str) or not \
+                authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ')[1]
