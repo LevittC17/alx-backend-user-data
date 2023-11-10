e7f45b07c7029abffa29c6577106d42a0696796c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-10 09:57:11 +0000

    class BasicAuth to inherit from Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Class inheritance
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Initialized a class to inherit from Auth Class
+    Returns:
+      - Empty for now
+    """
+    pass
