457e8fb783fa2eea82b28b2f3da52d7f9b7b9d11
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-10 09:50:10 +0000

    request validation on auth.require_auth method

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,32 +15,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
-if AUTH_TYPE == 'auth':
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth as AuthClass
     auth = AuthClass()
-elif AUTH_TYPE == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-
-@app.before_request
-def before_request() -> str:
-    """
-    Method to handle requests before processing the route
-    """
-    if auth is None:
-        return
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
-    if not auth.requires_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
@@ -62,6 +43,26 @@ def forbidden(error) -> str:
     """Authenticate Error Handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> str:
+    """
+    Method to handle requests before processing the route
+    """
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+    if auth:
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
