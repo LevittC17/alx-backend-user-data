cf5473d71ef3f83351df0c2c0f9f92c0cf35f01e
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-10 19:22:10 +0000

    Retrieve the User instance for a request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,7 +43,7 @@ class BasicAuth(Auth):
             decode_bytes = base64.b64decode(base64_authorization_header)
             decode_string = decode_bytes.decode('utf-8')
             return decode_string
-        except base64.binascii.Error:
+        except (base64.binascii.Error, UnicodeDecodeError):
             return None
 
     def extract_user_credentials(self, decode_base64_authorization_header:
@@ -80,3 +80,28 @@ class BasicAuth(Auth):
         return None
 
         return users[0] if users else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request
+        """
+        if request is None or not hasattr(request, 'headers'):
+            return None
+
+        authorization_header = request.headers.get('Authorization')
+        if authorization_header is None:
+            return None
+
+        base64_authorization_header = self.extract_base64_authorization_header(authorization_header)
+        if base64_authorization_header is None:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(base64_authorization_header)
+        if decoded_header is None:
+            return None
+
+        user_email, user_password = self.extract_user_credentials(decoded_header)
+        if user_email is None or user_password is None:
+            return None
+
+        return self.user_object_from_credentials(user_email, user_password)
