e100fa293ec0e239b1bdf427d71e6079ec9b03b7
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-16 14:02:27 +0000

    End-to-End Integration tests

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,30 +1,101 @@
 #!/usr/bin/env python3
+
 """
-Main file
+main module
 """
-from db import DB
-from user import User
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+import requests
 
 
-my_db = DB()
+BASE_URL = 'http://localhost:5000'
+EMAIL = 'guillaume@holberton.io'
+PASSWD = 'b4l0u'
+NEW_PASSWD = 't4rt1fl3tt3'
 
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
 
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
+def register_user(email, password):
+    response = requests.post(
+        f'{BASE_URL}/users',
+        data={
+            'email': email,
+            'password': password})
+    assert response.status_code == 200, f'Registration failed: \
+                                         {response.text}'
 
-try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
 
-try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
+def log_in_wrong_password(email, password):
+    response = requests.post(
+        f'{BASE_URL}/sessions',
+        data={
+            'email': email,
+            'password': password})
+    assert response.status_code == 401, f'Login should fail with \
+                                         wrong password: {response.text}'
+
+
+def profile_unlogged():
+    response = requests.get(f'{BASE_URL}/profile')
+    assert response.status_code == 403, f'Profile should be inaccessibe \
+                                         without login: {response.text}'
+
+
+def log_in(email, password):
+    response = requests.post(
+        f'{BASE_URL}/sessions',
+        data={
+            'email': email,
+            'password': password})
+    assert response.status_code == 200, f'Login failed: {response.text}'
+    return response.cookies.get('session_id')
+
+
+def profile_logged(session_id):
+    response = requests.get(
+        f'{BASE_URL}/profile',
+        cookies={
+            'session_id': session_id})
+    assert response.status_code == 200, f'Profile retrieval failed: \
+                                         {response.text}'
+
+
+def log_out(session_id):
+    response = requests.delete(
+        f'{BASE_URL}/sessions',
+        cookies={
+            'session_id': session_id})
+    try:
+        assert response.status_code == 200, f'Logout failed: {response.text}'
+    except AssertionError as e:
+        print(e)
+        print(response.text)
+
+
+def reset_password_token(email):
+    response = requests.post(f'{BASE_URL}/reset_password',
+                             data={'email': email})
+    assert response.status_code == 200, f'Password reset token retrieval \
+                                         failed: {response.text}'
+    return response.json()['reset_token']
+
+
+def update_password(email, reset_token, new_password):
+    response = requests.put(
+        f'{BASE_URL}/reset_password',
+        data={'email': email, 'reset_token': reset_token,
+              'new_password': new_password},
+    )
+    assert response.status_code == 200, f'Password update failed: \
+                                         {response.text}'
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
