f98fa984f6834d5274a23d922227593d22cd5a30
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-16 08:36:23 +0000

    Get the reset password token for a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -130,5 +130,25 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+    Get the reset password token for a user
+    Request:
+        POST /reset_password
+        Form data: {'email': '<user email>'}
+    Response:
+        200 OK - {"email": "<user email>", "reset_token": "<reset token>"}
+        403 Forbidden - {"message": "Email not registered"}
+    """
+    try:
+        email = request.form['email']
+        reset_token = AUTH.get_reset_password_toke(email)
+        response_data = {'email': email, 'reset_token': reset_token}
+        return jsonify(response_data), 200
+    except ValueError:
+        return jsonify({'message': 'Email not registered'}), 403
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
