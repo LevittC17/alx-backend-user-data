90f28b2b8ea11420504a6ba3f224278d0aee0365
LevittC17 <LeewednesdayC17@protonmail.com>
2024-01-15 23:25:53 +0300

    password hashing using bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+User passwords should NEVER be stored in
+plain text in a database
+"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password for storing:
+    Args:
+        password (str): The password to hash
+    Returns:
+        bytes: The hashed pasword
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed
